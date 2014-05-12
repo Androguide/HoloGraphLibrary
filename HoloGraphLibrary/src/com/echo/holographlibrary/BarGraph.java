675e98989b539cb52b637c5f2575e60fdfce73e4
iamyjx <iamyjx@gmail.com>
2014-05-12 15:09:15 +0800

    Update BarGraph.java
    need to add this?
    shouldUpdata=true,when Bargraph#setBars

--- HoloGraphLibrary/src/com/echo/holographlibrary/BarGraph.java
+++ HoloGraphLibrary/src/com/echo/holographlibrary/BarGraph.java
@@ -64,6 +64,7 @@ public class BarGraph extends View {
 
     public void setBars(ArrayList<Bar> points) {
         this.points = points;
+        shouldUpdate = true;
         postInvalidate();
     }
 
