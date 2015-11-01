af48147aa43ccb458a6dfaef8c090a9c376b6130
firewood <karamaki@gmail.com>
2015-11-01 16:23:57 +0900

    set thickness to 30dp add icons

--- HoloGraphLibrarySample/src/com/echo/holographlibrarysample/PieFragment.java
+++ HoloGraphLibrarySample/src/com/echo/holographlibrarysample/PieFragment.java
@@ -23,6 +23,8 @@
 
 package com.echo.holographlibrarysample;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -40,18 +42,26 @@ public class PieFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View v = inflater.inflate(R.layout.fragment_piegraph, container, false);
 		PieGraph pg = (PieGraph)v.findViewById(R.id.piegraph);
+
+		float density = getResources().getDisplayMetrics().density;
+		float thickness = 30.0f * density;
+		pg.setThickness((int) thickness);
+
 		PieSlice slice = new PieSlice();
 		slice.setColor(Color.parseColor("#99CC00"));
 		slice.setValue(2);
+		slice.setIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon_triangle));
 		pg.addSlice(slice);
 		slice = new PieSlice();
 		slice.setColor(Color.parseColor("#FFBB33"));
 		slice.setValue(3);
+		slice.setIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon_square));
 		pg.addSlice(slice);
 		slice = new PieSlice();
 		slice.setColor(Color.parseColor("#AA66CC"));
 		slice.setValue(8);
-		pg.addSlice(slice);		
+		slice.setIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon_star));
+		pg.addSlice(slice);
 		
 		pg.setOnSliceClickedListener(new OnSliceClickedListener(){
 
